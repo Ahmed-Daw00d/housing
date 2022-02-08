d3dca998dc01bc8639c572da8803e7d5876dabe0
Ahmed-Daw00d <adawood331@gmail.com>
2022-02-08 19:24:25 +0200

    main

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:housing/models/user.dart' as model;
 import 'package:housing/resources/storage_methods.dart';
 
 class AuthMethod {
@@ -30,39 +31,42 @@ class AuthMethod {
             .uploadImageToStorage('profilePics', file, false);
 
         // add user to our database
-        await _firestore.collection('users').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        model.User user = model.User(
+          username: username,
+          uid: cred.user!.uid,
+          email: email,
+          bio: bio,
+          followers: [],
+          following: [],
+          photoUrl: photoUrl,
+        );
+        await _firestore.collection('users').doc(cred.user!.uid).set(
+              user.toJson(),
+            );
 
         res = "success";
       }
-    }
-     catch (err) {
+    } catch (err) {
       res = err.toString();
     }
     return res;
   }
 
   //logging in user
-Future<String>loginUser({required String email,required String password})async{
-    String res="some error occurred";
+  Future<String> loginUser(
+      {required String email, required String password}) async {
+    String res = "some error occurred";
     try {
-      if(email.isNotEmpty||password.isNotEmpty) {
-        await _auth.signInWithEmailAndPassword(email: email, password: password);
-        res ="success";
-      }else{
-        res="please enter all the fields";
+      if (email.isNotEmpty || password.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        res = "success";
+      } else {
+        res = "please enter all the fields";
       }
-
-    }catch(err){
-      res=err.toString();
+    } catch (err) {
+      res = err.toString();
     }
     return res;
-}
+  }
 }
--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -5,7 +5,7 @@ class WebScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(child: Text("this is web")),
     );
   }
